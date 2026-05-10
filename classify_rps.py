15216e0ae140fe4d18bd0bd07ec3f4d6495fe42d
zonarr <poomison2013@gmail.com>
2026-05-11 00:27:30 +0700

    Add YOLO model weights: best.pt and last.pt

--- classify_rps.py
+++ classify_rps.py
@@ -1,5 +1,3 @@
 from ultralytics import YOLO
-#Loading pre-trained YOLO model for hand detection.
-#Data from https://universe.roboflow.com/handdetect-p7kf9/hand-detection-1d18n
-model = YOLO("yolo26n.pt")
-results = model.train(data="Hand Detection.v4i.yolov8/data.yaml",epochs=80,imgsz=640)
+#Model trained at https://colab.research.google.com/drive/1ayYObnWQFUs0NDX_SUrDKxztI_WQ0x9A?usp=sharing
+
